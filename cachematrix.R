7f23b1010f2e117f1c6026b28f163307622e4b83
Badalsoni <65660845+Badalsoni@users.noreply.github.com>
2020-05-20 21:21:37 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,16 +8,17 @@
 ## inverse from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-## returns matrix i.e. the inverse of 'x'
-        inv <- NULL
-        Set <- function(y){
-                x <<- y
-                inv <<- NULL
+
+inv <- NULL
+set <- function(y) {
+x <<- y
+inv <<- NULL
 }
-get <- function(y) {
-        setinv <- function(inverse) inv <<- inverse
-        getinv <- function() inv
-        list(set = set, get=get, setinv = setinv, getinv = getinv)
+get <- function() x
+setinv <- function(inverse) inv <<- inverse
+getinv <- function() inv
+list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
 
 ## Write a short comment describing this function
 ## "cacheSolve" is basically the function that computes the inverse of the special matrix returned by
@@ -25,13 +26,24 @@ get <- function(y) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-         inv <- x$getinv()
-        if(!is.null(inv)) {
-                message("getting cached output")
-                return(inv)
-        }
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setinv(inv)
-        inv
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached result")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
+        
+## ---------------Checking the program------------------------
+ m <- matrix(rnorm(16),4,4)
+ m1 <- makeCacheMatrix(m)
+cacheSolve(m1)
+
+##           [,1]       [,2]       [,3]       [,4]
+##[1,]  1.7107659  0.5705967 -1.0597085 -0.7598147
+##[2,] -4.2885292 -1.5347577  3.5950312  4.0708751
+##[3,]  1.5342610  0.7168858 -1.6785695 -1.2112209
+##[4,]  0.3030741 -0.8524399  0.0189958  0.1534455
